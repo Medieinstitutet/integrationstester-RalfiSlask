464563b07ea6200651a55c7e5452d4d7cfef55f8
RalfiSlask <nilssonnarki@gmail.com>
2024-05-21 14:31:24 +0200

    ♻️ remove unneccesary console.logs and import mocked movies

--- src/ts/__tests__/functions.test.ts
+++ src/ts/__tests__/functions.test.ts
@@ -1,44 +1,15 @@
 import { movieSort } from '../functions';
+import { moviesMock } from '../__mocks__/moviesMock';
 import { IMovie } from '../models/Movie';
 
 describe('#Sort function', () => {
-  let movies: IMovie[] = [];
+  let movies: IMovie[];
   let descTrue: boolean;
   let descFalse: boolean;
   beforeEach(() => {
     descTrue = true;
     descFalse = false;
-    movies = [
-      {
-        Title: 'Alien',
-        imdbID: '1',
-        Type: 'horror',
-        Poster: '/image1.webp',
-        Year: '1988',
-      },
-      {
-        Title: 'Batman',
-        imdbID: '2',
-        Type: 'action',
-        Poster: '/image1.webp',
-        Year: '1988',
-      },
-
-      {
-        Title: 'Cabin Fever',
-        imdbID: '3',
-        Type: 'horror',
-        Poster: '/image2.webp',
-        Year: '2000',
-      },
-      {
-        Title: 'Batman',
-        imdbID: '4',
-        Type: 'action',
-        Poster: '/image1.webp',
-        Year: '1988',
-      },
-    ];
+    movies = [...moviesMock];
   });
 
   test('movies should be sorted by titles in descending order', () => {
@@ -73,7 +44,6 @@ describe('#Sort function', () => {
 
   test('movies with equal title should maintain order', () => {
     movieSort(movies, descFalse);
-    console.log('movies:', movies);
     expect(movies[1].imdbID).toEqual('2');
     expect(movies[2].imdbID).toEqual('4');
   });
