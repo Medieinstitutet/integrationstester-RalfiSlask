9228d566502d21294dd0a0f12bda72f03758d3db
RalfiSlask <nilssonnarki@gmail.com>
2024-05-21 14:30:52 +0200

    🚚 create and move the mocked movies to its own mock file

---
+++ src/ts/__mocks__/moviesMock.ts
@@ -0,0 +1,33 @@
+import { IMovie } from '../models/Movie';
+
+export const moviesMock: IMovie[] = [
+  {
+    Title: 'Alien',
+    imdbID: '1',
+    Type: 'horror',
+    Poster: '/image1.webp',
+    Year: '1988',
+  },
+  {
+    Title: 'Batman',
+    imdbID: '2',
+    Type: 'action',
+    Poster: '/image1.webp',
+    Year: '1988',
+  },
+
+  {
+    Title: 'Cabin Fever',
+    imdbID: '3',
+    Type: 'horror',
+    Poster: '/image2.webp',
+    Year: '2000',
+  },
+  {
+    Title: 'Batman',
+    imdbID: '4',
+    Type: 'action',
+    Poster: '/image1.webp',
+    Year: '1988',
+  },
+];
